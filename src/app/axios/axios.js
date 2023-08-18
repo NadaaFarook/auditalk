c9e9a7c906087646e3c4820723c82d6390370aeb
Nada <nadaaofficial19@gmail.com>
2023-08-18 16:47:35 +0530

    backend

--- src/app/axios/axios.js
+++ src/app/axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   //baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
-  baseURL: "http://localhost:6010/api",
+  baseURL: "https://auditalk.hop.sh/api",
   headers: {
     Accept: "application/json",
     "Access-Control-Allow-Origin": "*",
