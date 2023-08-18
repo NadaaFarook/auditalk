11f326602183ce5a10a544508becefd0b8d9b1de
Nada <nadaaofficial19@gmail.com>
2023-08-18 16:38:49 +0530

    frontend setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,114 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import {
+  Flex,
+  Heading,
+  Container,
+  Grid,
+  Box,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AppointmentCard, Hero } from "./assets";
+import Image from "next/image";
+import { Footer, Navbar } from "./components";
 
-export default function Home() {
+const inter = Inter({ subsets: ["latin"] });
+
+const Home = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-inter: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <Navbar />
+      <Flex w={"100vw"} pos={"relative"}>
+        <Container maxW={"1440px"} margin="auto" py={24}>
+          <Grid templateColumns={"1fr 1fr"} alignItems={"center"}>
+            <Flex flexDir={"column"} gap={"20px"}>
+              <Heading fontSize={"6xl"} fontWeight={"700"} lineHeight={"1.3"}>
+                Professional{" "}
+                <Box display={"inline"} color={"#FFD700"}>
+                  {" "}
+                  Hearing Care{" "}
+                </Box>
+                at home.
+              </Heading>
+              <Text fontSize={"xl"} color={"#777777"}>
+                We Provide Hearing Checkup Right At Your Doorstep
+              </Text>
+            </Flex>
+            <Box w={"80%"} ml={"auto"}>
+              <Image src={Hero} />
+            </Box>
+          </Grid>
+        </Container>
+      </Flex>
+      <Flex w={"100vw"} pos={"relative"} bg={"#F8FAFC"}>
+        <Container maxW={"1440px"} margin="auto" py={24}>
+          <Heading
+            fontSize={"5xl"}
+            fontWeight={"600"}
+            lineHeight={"1.3"}
+            color={"#FFD700"}
+            textTransform={"uppercase"}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Quick Actions
+          </Heading>
+          <Box width={"80%"} ml={"auto"} borderTop={"1px solid #BCBCBC"}></Box>
+          <Grid display={['flex' , 'grid']} flexDir={'column'} templateColumns={"1fr 1fr"} gap={20} mt={20}>
+            {APPOINTMENT_CARDS.map((c) => {
+              return (
+                <Flex flexDir={"column"} gap={7} alignItems={"flex-start"}>
+                  <Image src={c.cardImage} />
+                  <Heading fontSize={"2xl"}>{c.title}</Heading>
+                  <Button
+                    as={"a"}
+                    display={{ base: "none", md: "inline-flex" }}
+                    fontSize={"lg"}
+                    fontWeight={400}
+                    color={"white"}
+                    bg={"#516EFF"}
+                    href={c.btnLink}
+                    _hover={{
+                      opacity: "80%",
+                    }}
+                    px={24}
+                    py={4}
+                  >
+                    {c.btnContent}
+                  </Button>
+                </Flex>
+              );
+            })}
+          </Grid>
+          {/* <Box pos={"absolute"} display={['none' , 'block']} left={"30px"} bottom={"30px"}>
+            <Image src={Line} />
+          </Box> */}
+        </Container>
+      </Flex>
+      <Footer />
+    </>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+const APPOINTMENT_CARDS = [
+  {
+    title: "Book an Appointment (home visit)",
+    btnContent: "Book Now",
+    btnLink: "/appointment",
+    cardImage: AppointmentCard,
+  },
+  {
+    title: "Book an Appointment (home visit)",
+    btnContent: "Book Now",
+    btnLink: "/appointment",
+    cardImage: AppointmentCard,
+  },
+];
+export default Home;
---
+++ time.md
@@ -0,0 +1,18 @@
+1hr - Navbar and Footer
+12:50 - 1:30 = 1hr
+
+1.5hr
+
+
+1. Landing page ka bas itna section
+2. Appointment 1(Address)
+3. Appointment (general / payment)
+4. Booking confirmation
+5. Sign in, Sign out (forgot password not required right now, so is OTP verification)
+6. Join us
+
+Click on Book Now in landing page -> opens Appointment 1(Address) page -> the go to Payment Page -> Payment Gateway -> Booking confirmation
+
+Call Now button in landing page will be CTA with phone number ofc
+
+join us ka form submit karne pe uske data ko google sheet aur db dono me store karna hai.
